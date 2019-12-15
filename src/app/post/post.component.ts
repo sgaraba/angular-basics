ee9b018b255858daa7b5b455f633cb23e075fa7b
Sergiu Garaba <Sergiu.Garaba@esempla.com>
2019-12-15 13:41:42 +0200

    Add component post, post-form

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,9 +1,18 @@
-import  {Component} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Post} from '../app.component';
 
 @Component({
-    selector: 'app-post',
-    templateUrl: './post.component.html',
-    styleUrls: ['./post.component.scss']
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent implements OnInit {
+
+  @Input() post: Post;
+
+  constructor() { }
+
+  ngOnInit() {
   }
-)
-export class PostComponent {}
+
+}
