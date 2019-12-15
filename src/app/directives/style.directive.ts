ccd379d0e0e851c2e11a26a5cb4e625375c4c236
Sergiu Garaba <Sergiu.Garaba@esempla.com>
2019-12-15 22:20:59 +0200

    acess element

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,10 +1,12 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef, private r: Renderer2) {
+    this.r.setStyle(el.nativeElement, 'color', 'blue');
+  }
 
 }
