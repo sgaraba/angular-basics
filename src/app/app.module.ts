25e03e3ac6c4d297058c544619382a3b66ac0b05
Sergiu Garaba <Sergiu.Garaba@esempla.com>
2019-12-22 21:54:11 +0200

    add custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
+import {MultByPipe} from './pipes/mult-by.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultByPipe
   ],
   imports: [
     BrowserModule,
