b6e3721b9b8e9d5531eeae55e98d2626e480bf8b
Sergiu Garaba <Sergiu.Garaba@esempla.com>
2019-12-23 21:29:02 +0200

    create group

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,18 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      email: new FormControl('', [
+          Validators.email, Validators.required
+        ]
+      ),
+      password: new FormControl(null, [
+          Validators.required, Validators.minLength(6)
+        ]
+      ),
+      address: new FormGroup({
+        country: new FormControl('ru'),
+        city: new FormControl('Moskow', Validators.required)
+      }),
     });
   }
 
