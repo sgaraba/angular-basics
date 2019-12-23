951ffc65b20e9168b7ef93440d7c2e9997775a7e
Sergiu Garaba <Sergiu.Garaba@esempla.com>
2019-12-23 14:12:44 +0200

    Create form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup;
 
+  ngOnInit() {
+    this.form = new FormGroup({});
+  }
+
+  submit() {
+    console.log('Form submited : ', this.form);
+  }
 }
