5b2e9666ee7df473e7d2204c741e427a51ec7f4a
Sergiu Garaba <Sergiu.Garaba@esempla.com>
2019-12-23 14:30:21 +0200

    Add validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +11,16 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(null),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
     });
   }
 
   submit() {
-    console.log('Form submited : ', this.form);
-    const formData = this.form.value;
-
-    console.log('Form data ; ', formData);
+    if (this.form.valid) {
+      console.log('Form submited : ', this.form);
+      const formData = this.form.value;
+      console.log('Form data ; ', formData);
+    }
   }
 }
